b68cddf8e14df702dd9d0c381a3745ae2ee3d699
fabio-genesis <fabio.franco@genesis-dataculture.com>
2025-09-09 10:00:17 -0300

    Alterar configuração de acesso público do bucket S3 para permitir acesso durante o desenvolvimento

--- main.tf
+++ main.tf
@@ -10,14 +10,16 @@ resource "aws_s3_bucket" "airflow_output" {
   }
 }
 
-# Bloqueia acesso público (segurança)
+# Acesso público (retirar em prd)
 resource "aws_s3_bucket_public_access_block" "airflow_output" {
   bucket                  = aws_s3_bucket.airflow_output.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
+
+
 
 ########################################
 # IAM role para ECS Task executar e escrever no S3
